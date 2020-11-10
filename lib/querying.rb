b13e5b5e746b5533683bc2a448e381270a9c87f4
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-11-10 05:36:56 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,20 +15,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  <<-SQL
-  SELECT series.title
-  FROM series
-  JOIN books 
-  ON series.id = books.series_id 
-  JOIN character_books 
-  ON books.id = character_books.book_id 
-  JOIN characters 
-  ON character_books.character_id = characters.id 
-  WHERE characters.species = 'human'
-  GROUP BY series.title 
-  ORDER BY COUNT(*) DESC
-  LIMIT 1
-  SQL
+  "SELECT series.title FROM series JOIN books ON series.id = book.series_id JOIN character_books ON books.id = character_books.book_id JOIN characters ON character_books.character_id = characters.id WHERE characters.species = 'human'GROUP BY series.title ORDER BY COUNT(*) DESC LIMIT 1"
   
   
   
